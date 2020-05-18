0c7789875bc9912130dc397b02be7d2aea06173b
harshitg123 <harshit.28.sps@gmail.com>
2020-05-18 11:49:53 +0530

    data types

---
+++ Data type's/Example.dart
@@ -0,0 +1,30 @@
+main()
+{
+  int no1 = 100;
+  var no2 = 200;
+
+  print("no1 is: $no1\nno2 is: $no2 \n");
+
+  String f_name = "Harshit";
+  var l_name = "Gupta";
+
+  print("My name is: $f_name $l_name\n");
+
+  double float_1 = 14.555;
+  var float_2 = 45.22;
+
+  print("f_1 is: $float_1\nf_2 is: $float_2\n");
+
+  bool check = true;
+  var post = false;
+
+  print("check post is: $check $post\n");
+
+  dynamic first_time = 100;
+
+    print("first_time is: $first_time");
+
+  first_time = "Dart programming";
+
+    print("Second_time is: $first_time\n");
+}
